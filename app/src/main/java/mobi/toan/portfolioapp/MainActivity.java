3253b43aae1a8b135971699b02da7597eda247fc
Toan Tran <toan@Toans-MacBook-Pro.local>
2015-06-01 20:15:11 +0700

    refactor - Removed  button declarations in MainActivity. - Used “onClick” tag in xml. - Used strings.xml to store messages. - Updated styles.

--- app/src/main/java/mobi/toan/portfolioapp/MainActivity.java
+++ app/src/main/java/mobi/toan/portfolioapp/MainActivity.java
@@ -11,66 +11,44 @@ import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
-
-    private Button mButtonSportifyStreamer;
-    private Button mButtonScoresApp;
-    private Button mButtonLibraryApp;
-    private Button mButtonBuildItBigger;
-    private Button mButtonBaconReader;
-    private Button mButtonCapstoneOwnApp;
+    private Toast mCurrentToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        initUI();
-    }
-
-    private void initUI() {
         setContentView(R.layout.activity_main);
-        mButtonSportifyStreamer = (Button) findViewById(R.id.button_sportify_streamer_app);
-        mButtonScoresApp = (Button) findViewById(R.id.button_scores_app);
-        mButtonBaconReader = (Button) findViewById(R.id.button_bacon_reader);
-        mButtonBuildItBigger = (Button) findViewById(R.id.button_build_it_bigger);
-        mButtonCapstoneOwnApp = (Button) findViewById(R.id.button_capstone_my_own_app);
-        mButtonLibraryApp = (Button) findViewById(R.id.button_library_app);
-
-        mButtonSportifyStreamer.setOnClickListener(mOnButtonClickListener);
-        mButtonScoresApp.setOnClickListener(mOnButtonClickListener);
-        mButtonBaconReader.setOnClickListener(mOnButtonClickListener);
-        mButtonBuildItBigger.setOnClickListener(mOnButtonClickListener);
-        mButtonCapstoneOwnApp.setOnClickListener(mOnButtonClickListener);
-        mButtonLibraryApp.setOnClickListener(mOnButtonClickListener);
     }
 
-    private View.OnClickListener mOnButtonClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            switch (v.getId()) {
-                case R.id.button_sportify_streamer_app:
-                    showMessage("Opening Sportify Stream app");
-                    break;
-                case R.id.button_scores_app:
-                    showMessage("Opening Score app");
-                    break;
-                case R.id.button_library_app:
-                    showMessage("Opening Library app");
-                    break;
-                case R.id.button_build_it_bigger:
-                    showMessage("Why not build it bigger!");
-                    break;
-                case R.id.button_bacon_reader:
-                    showMessage("Opening Bacon Reader app");
-                    break;
-                case R.id.button_capstone_my_own_app:
-                    showMessage("Opening Capstone my own app");
-                    break;
-                default:
-                    break;
-            }
+    public void onButtonClick(View button) {
+        switch (button.getId()) {
+            case R.id.button_sportify_streamer_app:
+                showMessage(getString(R.string.sportify_message));
+                break;
+            case R.id.button_scores_app:
+                showMessage(getString(R.string.score_app_message));
+                break;
+            case R.id.button_library_app:
+                showMessage(getString(R.string.library_app_message));
+                break;
+            case R.id.button_build_it_bigger:
+                showMessage(getString(R.string.build_it_bigger_message));
+                break;
+            case R.id.button_bacon_reader:
+                showMessage(getString(R.string.bacon_reader_message));
+                break;
+            case R.id.button_capstone_my_own_app:
+                showMessage(getString(R.string.capstone_app_message));
+                break;
+            default:
+                break;
         }
-    };
+    }
 
-    private void showMessage(String message ) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    private void showMessage(String message) {
+        if (mCurrentToast != null) {
+            mCurrentToast.cancel();
+        }
+        mCurrentToast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
+        mCurrentToast.show();
     }
 }
